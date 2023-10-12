e3ed0d84f3e599a788f305bcad208b445e6ef544
tsurkanoleg <tsurkanoleg1993@gmail.com>
2023-10-12 14:29:32 +0300

    created-react-twitter

---
+++ back/src/route/post.js
@@ -0,0 +1,145 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+const {Post} = require('../class/post')
+
+// ================================================================
+
+
+
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.post('/post-create', function (req, res) {
+	try {
+		const {username, text, postId} = req.body
+
+		if(!username || !text) {
+			return res.status(400).json({
+				message: 
+					"Потрібно передати всі дані для створення поста",
+			})
+		}
+
+		let post = null
+
+		console.log(postId, "postId")
+
+		if(postId) {
+			post = Post.getById(Number(postId))
+			console.log(post, "post")
+
+			if (!post) {
+				return res.status(400).json({
+					emssage: "Пост з таким id не існує",
+				})
+			}
+		}
+
+		const newPost = Post.create(username, text, post)
+
+		return res.status(200).json({
+		post: {
+			id: newPost.id,
+			test: newPost.text,
+			username: newPost.username,
+			date: newPost.date,
+		},
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message:e.message,
+		})
+	}		
+})
+
+// ================================================================
+
+router.get('/post-list', function (req, res) {
+	try {
+		const list = Post.getList()
+
+		if(list.length === 0) {
+			return res.status(200).json({
+				list: [],
+			})
+		}
+
+		return res.status(200).json({
+			list: list.map(({id, username, text, date}) => ({
+				id,
+				username,
+				text,
+				date,
+			})),
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message: e.message,
+		})
+
+
+		
+	}
+})
+
+// ================================================================
+
+router.get('/post-item', function (req, res) {
+	try {
+		const {id} = req.query
+
+		if(!id) {
+			return res.status(400).json({
+				message: "Потрібно передати ID поста"
+			})
+		}
+
+		const post = Post.getById(Number(id))
+
+		if(!post) {
+			return res.status(400).json({
+				message: "Пост з таким ID не існує"
+			})
+		}
+
+		return res.status(200).json({
+			post: {
+				id: post.id,
+				text: post.text,
+				username: post.username,
+				date: post.date,
+
+				reply: post.reply.map((reply) => ({
+					id: reply.id,
+					text: reply.text,
+					username: reply.username,
+					date: reply.date,
+				})),
+			},
+		})
+	} catch (e) {
+		return res.status(400).json({
+			message: e.message,
+		})
+
+
+		
+	}
+})
+
+// ================================================================
+
+
+
+// ================================================================
+
+
+
+// ================================================================
+
+
+
+// Підключаємо роутер до бек-енду
+module.exports = router
